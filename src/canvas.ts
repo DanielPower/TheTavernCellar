24418f95b5c70d5b364320177800f70ffb82728b
Daniel Power <me@danielpower.ca>
2023-08-07 00:24:44 -0230

    Simplify canvas sizing

--- src/canvas.ts
+++ src/canvas.ts
@@ -3,13 +3,12 @@ import Two from 'two.js';
 const canvas = document.getElementById('game') as HTMLCanvasElement;
 export const two = new Two({
 	type: Two.Types.canvas,
-	domElement: canvas
+	domElement: canvas,
+	width: 400,
+	height: 225,
 });
 
-const onResize = () => {
-	const width = canvas.parentElement!.clientWidth;
-	const height = width * (9 / 16);
-	two.renderer.setSize(width, height);
-};
-onResize();
-window.addEventListener('resize', onResize);
+// Wait for Two to resize the canvas, so I can undo its styling
+two.update();
+canvas.style.width = '';
+canvas.style.height = '';
