90586df337362c5f530b6831dc648184c767149b
Daniel Power <me@danielpower.ca>
2023-08-08 17:39:26 -0230

    Remove build folder

--- src/components/Cellar.svelte
+++ src/components/Cellar.svelte
@@ -7,9 +7,9 @@
 <div class="flex">
   <Box class="shrink-0 flex-col bg-green-950">
     <img src="cellar.webp" alt="Cellar" width="200" height="200" />
-    <Button on:click={store.manualKill} disabled={$store.energy === 0}
-      >Kill a rat</Button
-    >
+    <Button on:click={store.manualKill} disabled={$store.energy === 0}>
+      Kill a rat
+    </Button>
     {#if $store.kills >= 50}
       <Button on:click={store.gotoTavern}>Return to Tavern</Button>
     {/if}
