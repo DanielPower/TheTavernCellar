efe6e55701c730e761baebf58fdb072c00a64c45
Daniel Power <me@danielpower.ca>
2023-08-07 23:54:33 -0230

    Fix building

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.2531cec9.js","_app/immutable/chunks/scheduler.269df074.js","_app/immutable/chunks/index.f81231ab.js","_app/immutable/chunks/index.be147fa3.js"];
-export const stylesheets = [];
-export const fonts = [];
--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ FROM node:18-alpine as deploy
 WORKDIR /app
 RUN rm -rf ./*
 COPY --from=build /app/package.json .
-COPY --from=build /app/dist .
+COPY --from=build /app/build .
 RUN npm install --omit=dev
 EXPOSE 8080/tcp
 CMD ["npm", "exec", "http-server"]
